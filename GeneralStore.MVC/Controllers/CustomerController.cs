7e105f5a16b1934c4321d1e2840e9a6816e48055
7thedan <dannyan29@gmail.com>
2021-07-01 20:43:28 -0400

    Add project files.

---
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,127 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        private ApplicationDbContext _db = new ApplicationDbContext(); 
+
+        // GET: Restaurant/Index
+        public ActionResult Index()
+        {
+            return View(_db.Customers.ToList());
+        }
+
+        //GET; Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost] //only a post method.
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer); //doens't wipe all the information just bc you didnt write one line.
+        }
+
+        //GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id) //id not can be null. int is not nullable
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        //POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _db.Customers.Find(id);
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Customer/Edit/{id}
+        //Get an id from the user
+        //Handle if the id is null
+        //Find a Customer by id
+        //If the customer doesn't exit
+        //Return the customer and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            return View(customer);
+        }
+
+
+        //POST: Restaurant/Edit/{id}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(customer).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+
+        //GET: Restaurant/Details/{id}
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            return View(customer);
+        }
+
+    }
+}
---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        [Required]
+        [Display(Name = "Transaction ID")]
+        public int TransactionID { get; set; }
+        [ForeignKey(nameof(Customer))]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+        [ForeignKey(nameof(Product))]
+        public virtual Product Product { get; set; }
+        public int ProductID { get; set; }
+        public int Quanitity { get; set; }
+        public decimal TotalCost
+        {
+            get
+            {
+                return Product.UnitPrice * Quanitity;
+            }
+        }
+
+
+    }
+}
